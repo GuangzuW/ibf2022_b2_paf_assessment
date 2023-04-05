a86bb1860ab5f25ddac00c150a34c464a3b55a51
Wang Guangzu <wgz199154@hotmail.com>
2023-04-05 17:01:41 +0800

    commit

--- database/data.sql
+++ database/data.sql
@@ -5,3 +5,10 @@ insert into user(user_id,username,name) values("a8b9800d","barney","Barney");
 insert into user(user_id,username,name) values("66223e28","betty","Betty");
 
 select * from user;
+
+select * from user where username="fred";
+
+
+insert into task (task_id,description, priority, due_date, user_id,username) values (1,"test",2,"2023-05-04","1b80114c","fred");
+
+select * from task;
--- database/schema.sql
+++ database/schema.sql
@@ -10,12 +10,15 @@ CREATE TABLE user (
   constraint user_pk PRIMARY KEY (`user_id`)
 );
 
+drop table task;
+
 CREATE TABLE task (
   task_id INT NOT NULL AUTO_INCREMENT,
   description VARCHAR(255),
   priority INT,
   due_date DATE,
   user_id CHAR(8),
+  username VARCHAR(255),
   constraint task_pk PRIMARY KEY (task_id),
   constraint  task_user_pk FOREIGN KEY (user_id) REFERENCES user(user_id),
   CHECK (priority >= 1 AND priority <= 3)
